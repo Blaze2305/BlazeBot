20caa201bc29be1b674f73ae4568c8c5e0fb36c4
Pranav <prajpran@gmail.com>
2022-03-16 04:07:42 +0530

    fixed the bot and moved it to use replies instead; using mulitple emojis for some characters; added random chance to TWSS check

--- BlazeBot.py
+++ BlazeBot.py
@@ -1,48 +1,73 @@
 import discord
 from twss import TWSS
+import nltk
 from os import environ
+from random import random
 from discord.ext.commands import Bot
+from discord import DeletedReferencedMessage
 from re import compile
 from string import punctuation
+from random import random
+import asyncio
 
-# Dictionary of all letters
-letters = {
-	"A":"\U0001F1E6",
-	"B":"\U0001F1E7",
-	"C":"\U0001F1E8",
-	"D":"\U0001F1E9",
-	"E":"\U0001F1EA",
-	"F":"\U0001F1EB",
-	"G":"\U0001F1EC",
-	"H":"\U0001F1ED",
-	"I":"\U0001F1EE",
-	"J":"\U0001F1EF",
-	"K":"\U0001F1F0",
-	"L":"\U0001F1F1",
-	"M":"\U0001F1F2",
-	"N":"\U0001F1F3",
-	"O":"\U0001F1F4",
-	"P":"\U0001F1F5",
-	"Q":"\U0001F1F6",
-	"R":"\U0001F1F7",
-	"S":"\U0001F1F8",
-	"T":"\U0001F1F9",
-	"U":"\U0001F1FA",
-	"V":"\U0001F1FB",
-	"W":"\U0001F1FC",
-	"X":"\U0001F1FD",
-	"Y":"\U0001F1FE",
-	"Z":"\U0001F1FF"
-}
+TWSS_CHANCE = 1/int(environ.get("TWSS_CHANCE","10"))
+
+
+
+def get_reaction_message(message:str)->list:
+	'''
+		Convert all the chars in the message to reactable emojis that discord can use.
+		NOTE: if the message contains chars that are not in the letters dict or if the same letter is used more times 
+		in the message than the number of emojis available for that character, it doesn't fail but just doesn't do anything with it
+	'''
+	
+	# Dictionary of all letters
+
+	letters = {
+		"A":["🇦","🅰️"],
+		"B":["🇧","🅱️"],
+		"C":["🇨"],
+		"D":["🇩"],
+		"E":["🇪"],
+		"F":["🇫"],
+		"G":["🇬"],
+		"H":["🇭"],
+		"I":["🇮","ℹ️"],
+		"J":["🇯"],
+		"K":["🇰"],
+		"L":["🇱"],
+		"M":["🇲","Ⓜ️"],
+		"N":["🇳"],
+		"O":["🇴","🅾️","⭕","0️⃣"],
+		"P":["🇵","🅿️"],
+		"Q":["🇶"],
+		"R":["🇷"],
+		"S":["🇸"],
+		"T":["🇹"],
+		"U":["🇺"],
+		"V":["🇻"],
+		"W":["🇼"],
+		"X":["🇽","❌","❎"],
+		"Y":["🇾"],
+		"Z":["🇿"]
+	}
+
+	output = []
+	for char in message:
+		emojis = letters.get(char.upper(),None)
+
+		if emojis:
+			output.append(emojis.pop(0))
+	return output
 
 # Filter out of the duplicate chars in the word without messing up the order
-def filter_duplicates(seq:str):
+def filter_duplicates(seq:str)->str:
 	seen = set()
 	seen_add = seen.add
-	return [x for x in seq if not (x in seen or seen_add(x))]
+	return "".join([x for x in seq if not (x in seen or seen_add(x))])
 
 # Clean the sentence from all punctuation
-def clean_sentence(sentence:str):
+def clean_sentence(sentence:str)->str:
 	pattern = compile(f"[{punctuation}]")
 	sentence = pattern.sub("",sentence)
 	return sentence
@@ -61,48 +86,67 @@ async def on_ready():
 @bot.event
 async def on_message(message : discord.Message):
 	if message.content[0]!="&":
-		sentence = clean_sentence(message.content)
-		if twssBot(sentence) and message.author != bot.user:
-			response = f"> {message.content}\n{message.author.mention} Thats what she said ;D"
-			await message.channel.send(response)
+		chance =  random()
+		print(chance,TWSS_CHANCE)
+		if chance< TWSS_CHANCE:
+			sentence = clean_sentence(message.content)
+			if twssBot(sentence) and message.author != bot.user:
+				response = f"> {message.content}\n{message.author.mention} Thats what she said ;D"
+				await message.channel.send(response)
 	else:
-		await bot.process_commands(message)
+		try:
+			await bot.process_commands(message)
+		except discord.Forbidden:
+				response = f"{message.author.mention} I do not have permission to send and/or react in this channel."
+				await message.channel.send(response)
+
 
 @bot.command(pass_context=True)
 async def wr(ctx : discord.ext.commands.Context):
 
-	# Gets the message to be reacted to
-	try:
-		quoted = ctx.message.content.split("\n")[1]
-	except IndexError as err:
-		await ctx.channel.send(f"{ctx.message.author.mention} You forgot to quote the message you want to react to")
-		return
-	# Remove the bit that makes the message quoted
-	quoted = quoted.replace("> ","")
-	# Get the word to be reacted
-	try:
-		word = ctx.message.content.split("\n")[2]
-	except Exception as e:
-		await ctx.channel.send("Enter the text to be reacted")
-	word = word.split(" ")
-	# Get the author of the original message
-	author = word.pop(0)
-	# Remove the space and create the word
-	word = "".join(word)
-	word = filter_duplicates(word)
-	# Get all the past 100 messages in the channel
-	async for i in ctx.channel.history(limit=100):
-		# Check if the quoted text is the same and if its from the same user
-		if quoted == i.content and int(author[3:-1]) == i.author.id:
-			# Fetch the message if it matches
-			message = await ctx.channel.fetch_message(i.id)
-			# Clear all previous reactions
-			await message.clear_reactions()
-			# React to the message char by char
-			for char in word:
-				# Add each char as a reaction
-				await message.add_reaction(letters[char.upper()])
-			# exit
-			return
+	message =  ctx.message.reference
 
-bot.run(environ['BLAZE_BOT'])
+	# Gets the message to be reacted to
+	if not message:
+		await ctx.channel.send(f"{ctx.message.author.mention} You forgot to reply to the message you want to react to")
+		return
+	else:
+		word =  ctx.message.clean_content[4:]
+		
+		if len(word)>20:
+			await ctx.message.author.send("Your message was more than 20 characters long. Due to discord limitations, only the first 20 characters will be used for the reaction")
+
+		word =  word[:20]
+		
+	# word = filter_duplicates(word)
+
+	replied_message = message.resolved
+
+	if(isinstance(replied_message,DeletedReferencedMessage)):
+		await ctx.channel.send(f"{ctx.message.author.mention} The message you replied to no longer exists ;-;")
+		return
+
+	# Clear all previous reactions
+	await replied_message.clear_reactions()
+
+	# get the list of emojis that can be used to form the message
+	emoji_list = get_reaction_message(word)
+
+	print(emoji_list)
+
+	# React to the message emoji by emoji
+	for emoji in emoji_list:
+		# Add each emoji as a reaction
+		await replied_message.add_reaction(emoji)
+
+	await ctx.message.delete()
+	# exit
+	return
+
+def install_nlkt_packages():
+	nltk.download("punkt")
+
+
+if __name__ == "__main__":
+	install_nlkt_packages()
+	bot.run(environ['BLAZE_BOT_TOKEN'])
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # React-Bot
+
+This bot has two main functions :
+
+- A "That's what she said" replier, thing which replies that what she said to certain sentences. I've set this to run for every 1 in X messages cuz I don't want it to be too annoying. But that can be changes by the env variable TWSS_CHANCE. If its not specified it default to 1 in 10.
+- A message reaction bot to react to messages with some text. An example is shown below.
+
+![Example reaction](assets/example.png)
+## To run:
+
+You'll need some libraries to run this, so install them using 
+
+```bash
+$ pip install requirements.txt
+```
+
+You'll also need to set two environment variables
+
+- ```BLAZE_BOT_TOKEN``` : The discord bot token 
+- ```TWSS_CHANCE``` : an integer value `X`, which gives the bot a  `1 in X` chance to reply `That's what she said`.
+
+You can then locally run the bot by running
+
+```bash
+$ python BlazeBot.py
+```
